c08215fa71be64db889ae1b76177c0d258b52298
David Vilaça <david@xdevel.com.br>
2018-11-07 15:40:21 -0200

    fix: add environment variable compose url

--- src/core/environments.ts
+++ src/core/environments.ts
@@ -4,6 +4,7 @@ config()
 
 export const environments = {
   gerencioUrl: String(process.env.GERENCIO_URL),
+  gerencioComposeUrl: String(process.env.GERENCIO_COMPOSE_URL),
   gerencioAccessKey: String(process.env.GERENCIO_ACCESS_KEY),
   gerencioSecretKey: String(process.env.GERENCIO_SECRET_KEY),
   gerencioStack: String(process.env.GERENCIO_STACK)
--- src/interfaces/deploy-envs.ts
+++ src/interfaces/deploy-envs.ts
@@ -1,5 +1,6 @@
 export interface IDeployEnvs {
   gerencioUrl: string
+  gerencioComposeUrl: string
   gerencioAccessKey: string
   gerencioSecretKey: string
   gerencioStack: string
